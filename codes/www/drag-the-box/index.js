0fa10847d3ebdf8a9f64ef96d3ad6910f67a7405
Nagender211 <dnagender2019@gmail.com>
2024-05-04 11:03:21 +0530

    updated the drag-the-box

--- codes/www/drag-the-box/index.js
+++ codes/www/drag-the-box/index.js
@@ -6,7 +6,9 @@ function drag() {
     const dragPlaySound=document.getElementById("dragPlaySound");
     const dragPlaySound1=document.getElementById("dragPlaySound1");
     const dragPlaySound2=document.getElementById("dragPlaySound2");
-    dragPlaySound.volume=0.0009;
+    const dragPlaySound3=document.getElementById("dragPlaySound3");
+    const dragPlaySound4=document.getElementById("dragPlaySound4");
+    // dragPlaySound.volume=0.0009;
 
     boxes.forEach(box => {
         box.addEventListener("mousedown", mouseDown);
@@ -37,13 +39,103 @@ function drag() {
             const DMY = e.pageY - mouseY;
             dragging.style.left = eleX + DMX + "px";
             dragging.style.top = eleY + DMY + "px";
+           
 
             
             const coordDisplay = document.getElementById("coordDisplay");
-            const leftDragged=coordDisplay.innerText = `Left: ${eleX + DMX}px`;
-            const topDragged=coordDisplay.innerText += `\nTop: ${eleY + DMY}px`;
-            dragPlaySound2.play();
+            coordDisplay.innerText = `Left: ${eleX + DMX}px`;
+            coordDisplay.innerText += `\nTop: ${eleY + DMY}px`;
+           
+            if (DMX+eleX <= 100) {
+                dragPlaySound2.volume=0.03;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }else if(DMX+eleX > 101 && DMX+eleX <=200) {
+                dragPlaySound2.volume=0.3;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 201 && DMX+eleX <=250) {
+                dragPlaySound2.volume=0.9;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if (DMX+eleX >= 251 && DMX+eleX <=350) {
+                dragPlaySound1.volume=0.05;
+                dragPlaySound1.play();
+                dragPlaySound.pause();
+                dragPlaySound2.pause();
 
+            }else if (DMX+eleX >= 351 && DMX+eleX <=450) {
+                dragPlaySound1.volume=0.5;
+                dragPlaySound1.play();
+                dragPlaySound.pause();
+                dragPlaySound2.pause();
+
+            }
+            else if (DMX+eleX >= 451 && DMX+eleX <=500) {
+                dragPlaySound1.volume=0.999;
+                dragPlaySound1.play();
+                dragPlaySound.pause();
+                dragPlaySound2.pause();
+
+            }
+            else if (DMX+eleX >= 501 && DMX+eleX <=600) {
+                dragPlaySound3.volume=0.05;
+                dragPlaySound3.play();
+                dragPlaySound.pause();
+                dragPlaySound1.pause();
+                dragPlaySound2.pause();
+
+            }else if (DMX+eleX >= 601 && DMX+eleX <=700) {
+                dragPlaySound3.volume=0.5;
+                dragPlaySound3.play();
+                dragPlaySound.pause();
+                dragPlaySound1.pause();
+                dragPlaySound2.pause();
+
+            }
+            else if (DMX+eleX >= 701 && DMX+eleX <=750) {
+                dragPlaySound3.volume=0.99;
+                dragPlaySound3.play();
+                dragPlaySound.pause();
+                dragPlaySound1.pause();
+                dragPlaySound2.pause();
+
+            }
+            else if (DMX+eleX >= 751 && DMX+eleX <=850) {
+                dragPlaySound4.volume=0.05;
+                dragPlaySound4.play();
+                dragPlaySound.pause();
+                dragPlaySound1.pause();
+                dragPlaySound2.pause();
+
+            } else if (DMX+eleX >= 851 && DMX+eleX <=950) {
+                dragPlaySound4.volume=0.5;
+                dragPlaySound4.play();
+                dragPlaySound.pause();
+                dragPlaySound1.pause();
+                dragPlaySound2.pause();
+
+            }else if (DMX+eleX >= 951 && DMX+eleX <=1050) {
+                dragPlaySound4.volume=0.99;
+                dragPlaySound4.play();
+                dragPlaySound.pause();
+                dragPlaySound1.pause();
+                dragPlaySound2.pause();
+
+            }
+
+            else{
+                dragPlaySound.volume=0.999999999999999999999999999999999999999999999999999999;
+                dragPlaySound.play();
+            }
         }
       
     }
@@ -52,8 +144,7 @@ function drag() {
         dragging = null;
         document.removeEventListener('mousemove', mouseMove);
         document.removeEventListener('mouseup', mouseUp);
-        dragPlaySound.play();
-
+      
         
     }
 }
