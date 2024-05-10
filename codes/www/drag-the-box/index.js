601db873b982da2c837a864bac2f510171e2a43d
Nagender211 <dnagender2019@gmail.com>
2024-05-11 00:53:58 +0530

    updated the darg-the-box

--- codes/www/drag-the-box/index.js
+++ codes/www/drag-the-box/index.js
@@ -3,15 +3,16 @@ function drag() {
     let mouseX, mouseY;
     let eleX, eleY;
     const boxes = document.querySelectorAll("[draggable]");
-    const dragPlaySound=document.getElementById("dragPlaySound");
-    const dragPlaySound1=document.getElementById("dragPlaySound1");
-    const dragPlaySound2=document.getElementById("dragPlaySound2");
-    const dragPlaySound3=document.getElementById("dragPlaySound3");
-    const dragPlaySound4=document.getElementById("dragPlaySound4");
+    const dragPlaySound = document.getElementById("dragPlaySound");
+    const dragPlaySound1 = document.getElementById("dragPlaySound1");
+    const dragPlaySound2 = document.getElementById("dragPlaySound2");
+    const dragPlaySound3 = document.getElementById("dragPlaySound3");
+    const dragPlaySound4 = document.getElementById("dragPlaySound4");
     // dragPlaySound.volume=0.0009;
 
     boxes.forEach(box => {
         box.addEventListener("mousedown", mouseDown);
+        box.addEventListener("touchstart", touchStart); // Added touch event listener
         box.style.position = 'absolute';
         box.style.top = 0;
         box.style.left = 0;
@@ -28,24 +29,35 @@ function drag() {
 
         document.addEventListener('mousemove', mouseMove);
         document.addEventListener('mouseup', mouseUp);
-       
-      
-        
+    }
+
+    function touchStart(e) {
+        e.preventDefault();
+        dragging = this;
+        mouseX = e.touches[0].pageX;
+        mouseY = e.touches[0].pageY;
+
+        eleX = dragging.offsetLeft;
+        eleY = dragging.offsetTop;
+
+        document.addEventListener('touchmove', touchMove);
+        document.addEventListener('touchend', touchEnd);
     }
 
     function mouseMove(e) {
         if (dragging) {
             const DMX = e.pageX - mouseX;
             const DMY = e.pageY - mouseY;
-            dragging.style.left = eleX + DMX + "px";
-            dragging.style.top = eleY + DMY + "px";
-           
+            const windowWidth = window.innerWidth;
 
-            
+            const maxPosX = windowWidth - dragging.offsetWidth;
+            const posX = eleX + DMX;
+            dragging.style.left = posX <= maxPosX ? Math.max(0, posX) + "px" : maxPosX + "px";
+            dragging.style.top = eleY + DMY + "px";
+
             const coordDisplay = document.getElementById("coordDisplay");
-            coordDisplay.innerText = `Left: ${eleX + DMX}px`;
+            coordDisplay.innerText = `Left: ${parseInt(dragging.style.left)}px`;
             coordDisplay.innerText += `\nTop: ${eleY + DMY}px`;
-           
             if (DMX+eleX <= 100) {
                 dragPlaySound2.volume=0.03;
                 dragPlaySound2.play();
@@ -137,22 +149,97 @@ function drag() {
                 dragPlaySound.play();
             }
         }
-      
+    }
+
+    function touchMove(e) {
+        if (dragging) {
+            const DMX = e.touches[0].pageX - mouseX;
+            const DMY = e.touches[0].pageY - mouseY;
+            const windowWidth = window.innerWidth;
+
+            const maxPosX = windowWidth - dragging.offsetWidth;
+            const posX = eleX + DMX;
+            dragging.style.left = posX <= maxPosX ? Math.max(0, posX) + "px" : maxPosX + "px";
+            dragging.style.top = eleY + DMY + "px";
+
+            const coordDisplay = document.getElementById("coordDisplay");
+            coordDisplay.innerText = `Left: ${parseInt(dragging.style.left)}px`;
+            coordDisplay.innerText += `\nTop: ${eleY + DMY}px`;
+            if (DMX+eleX <= 100) {
+                dragPlaySound2.volume=0.3;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }else if(DMX+eleX > 101 && DMX+eleX <=150) {
+                dragPlaySound2.volume=0.5;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 151 && DMX+eleX <=220) {
+                dragPlaySound2.volume=0.9;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 221 && DMX+eleX <=300) {
+                dragPlaySound2.volume=0.8;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 301 && DMX+eleX <=400) {
+                dragPlaySound2.volume=0.3;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 401 && DMX+eleX <=550) {
+                dragPlaySound2.volume=0.5;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 551 && DMX+eleX <=650) {
+                dragPlaySound2.volume=0.7;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+            else if(DMX+eleX > 651 && DMX+eleX <=1110) {
+                dragPlaySound2.volume=0.99999;
+                dragPlaySound2.play();
+                dragPlaySound1.pause();
+                dragPlaySound.pause();
+               
+            }
+        }
     }
 
     function mouseUp() {
         dragging = null;
         document.removeEventListener('mousemove', mouseMove);
         document.removeEventListener('mouseup', mouseUp);
-      
-        
     }
+
+    function touchEnd() {
+        dragging = null;
+        document.removeEventListener('touchmove', touchMove);
+        document.removeEventListener('touchend', touchEnd);
+    }
+
+    const windowHeight = window.innerHeight;
+    const windowWidth = window.innerWidth;
+
+    console.log("Window height:", windowHeight);
+    console.log("Window width:", windowWidth);
 }
 
 drag();
-const windowHeight = window.innerHeight;
-const windowWidth = window.innerWidth;
-
-console.log("Window height:", windowHeight);
-console.log("Window width:", windowWidth);
-
