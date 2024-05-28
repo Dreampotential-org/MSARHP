833ab214e9762b51cc2a9713fb0be2e3ec86287a
Nagender211 <dnagender2019@gmail.com>
2024-05-28 08:53:42 +0530

    updated statsjs

--- codes/www/js/stats.js
+++ codes/www/js/stats.js
@@ -1,17 +1,21 @@
 var POINTS = []
-
+let total_miles=document.getElementById('total_miles')
 function session_point(position)  {
     point = {
 	'latitude':  position.coords.latitude,
         'longitude': position.coords.longitude,
         'created_at': new Date()
     }
+    
+     
+
 
     POINTS.push(point)
 
     if (POINTS.length == 1) {
         add_point("map_canvas", point, true)
 	     //alert("DJDJDJ")
+         
     }
     add_point("map_canvas", point, false)
     session_point_api(position)
@@ -32,6 +36,7 @@ function localstats(position)  {
     }
 
     // update total distance
+    console.log(getspdistance(POINTS));
     totaldistance += get_distance(  
 	POINTS[ POINTS.length -2 ]['latitude'],
 	POINTS[ POINTS.length -2 ]['longitude'],
@@ -112,3 +117,104 @@ function get_local_stats() {
     console.log({distance});
     console.log({POINTS});
 }
+function getspdistance(session_points){
+    if(!session_points){
+        return {'distance_miles': 0,
+                'distance_meters': 0,
+                'interval_stats': []}
+    }
+    let interval_distance=0;
+    let interval_stats = [];
+    let session_distance = 0;
+    let complete_one_mile = 0;
+    let speed_per_mile_cover_last = 0;
+    let start_session;
+    let distance;
+    for (var i = 0; i < session_points.length -1; i++) {
+        if(start_session == null){
+            start_session = session_points[i]
+
+        }
+        distance = get_distance(
+            session_points[i].latitude, session_points[i].longitude,
+            session_points[i + 1 ].latitude, session_points[i +1].longitude
+        )
+        if(complete_one_mile == 0){
+            speed_cover_per_mile = session_points[i].created_at
+
+        }
+        session_distance += distance
+        interval_distance += distance
+        complete_one_mile += distance
+        if (0.62137 * interval_distance >= .1){
+             hours = float(
+                (start_session.created_at - session_points[i].created_at).seconds/
+                (60 * 60)
+            )
+             mph = (
+                (0.62137 * interval_distance) / hours
+            )
+
+            start_session = session_points[i]
+            interval_stats.append({
+                'distance': interval_distance,
+                'mph': mph,
+                'hours': hours,
+                'speedFlow': 'none'
+            })
+
+            interval_distance = 0
+
+            
+        }
+               
+    if (0.62137 * complete_one_mile >= 1){
+         hours = float(
+             (speed_cover_per_mile - session_points[i].created_at).seconds/
+             (60 * 60)
+         )
+          mph = (
+             (0.62137 * complete_one_mile) / hours
+         )
+         if(speed_per_mile_cover_last == 0){
+             speed_type = speedFlow[0]
+         }
+             
+         else if(speed_per_mile_cover_last > mph){
+             speed_type = speedFlow[2];
+         }
+             
+        else if(speed_per_mile_cover_last < mph){
+                 speed_type = speedFlow[3]
+
+             }
+           
+
+         speed_per_mile_cover_last = mph
+
+         interval_stats.append({
+             'distance': interval_distance,
+             'mph': mph,
+             'hours': hours,
+             'speedFlow': speed_type
+         })
+         complete_one_mile = 0
+        }
+       
+
+    }
+    const data= {'distance_miles': session_distance * 0.62137,
+    'distance_meters': session_distance * 1000,
+    'interval_stats': interval_stats,
+    "POINTS": POINTS.length,
+    
+}   
+    return JSON.stringify(data);
+    
+    
+
+   
+
+    
+}
+
